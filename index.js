218efa8dc161a8c79b3acc6a8f74050e039a2628
+ <+>
2022-08-11 05:43:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, surpise) {
+	const thankYous = [];
+	for (let i = 0; i < names.length; i++) {
+		const thankYou = `Thank you, ${names[i]}, for the wonderful ${surpise} gift!`;
+		thankYous.push(thankYou);
+	}
+	return thankYous;
+}
+
+writeCards(names, "surprise");
+
+const number = 10;
+function countDown(number) {
+	let i = 10;
+	while (i >= 0) {
+		console.log(i--);
+	}
+	return number;
+
+}
+countDown(number);
